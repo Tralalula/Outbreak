0ecc7d6005e31c9ae6750992dec6004953e98b22
Tobias <qenzoz@gmail.com>
2016-01-12 14:34:09 +0100

    Add stub classes

---
+++ core/src/com/mygdx/game/Constants.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Color;
+
+/**
+ * Created by Tobias on 1/12/2016.
+ */
+public class Constants {
+    // Game
+    public static final float WORLD_SIZE = 10.0f;
+    public static final Color BACKGROUND_COLOR = Color.BLACK;
+
+    // Paddle
+
+    // Bricks
+
+    // Ball
+}
---
+++ core/src/com/mygdx/game/Paddle.java
@@ -0,0 +1,8 @@
+package com.mygdx.game;
+
+/**
+ * Created by Tobias on 1/12/2016.
+ */
+public class Paddle {
+
+}
